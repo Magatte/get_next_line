f477bf25328eb78d3249e07e7943d2a1b01168d6
Magatte BA <pba@e3r4p10.42.fr>
2016-05-12 01:48:01 +0200

    get_next_line pushed

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pba <pba@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/12/13 18:16:20 by pba               #+#    #+#             */
+/*   Updated: 2016/03/15 16:25:39 by pba              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft.h"
+
+# define READ_MAX 40
+
+int		get_next_line(int const fd, char **line);
+
+#endif
